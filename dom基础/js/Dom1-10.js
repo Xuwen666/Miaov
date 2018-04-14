b83fae17f500f167402d2ec1a80204733b9239a5
xuwen6662 <1956957897@qq.com>
2018-04-14 18:00:24 +0800

    4-14 18:00

--- dom基础/js/Dom1-10.js
+++ dom基础/js/Dom1-10.js
@@ -6,29 +6,101 @@
  */
 
 /*
-	功能点1.
-  姓名 性别  年龄
-  点击提交按钮 获取到 姓名 性别 年龄三个的值添加到dom元素中
-  			每次添加到dom中都要刷新一次所有的tr元素目的是为了隔行变色以及获取tr的值 拿来赋值给编号
- 
- 功能点2. 
-  编号 年龄 升序 降序
-  点击排序按钮 
-  			   根据  select第一个值进行判断是升序还是降序
-				判断是编号还是年龄 获取到编号或年龄的最大值传入
-			
-			重新渲染tr的顺序
+ 	京东在布局上面这个需求上面的布局嵌套的很多层 例如条件选项区域代码如下
+ 	div>div>ul>li>a  大体结构是这样的,除此之外京东用了多个div,目的应该是为了兼容低版本的ie
+	
+	从json中拿到数据 先遍历这个数组在html中创建结构
+
+	一开始有一种思路就是在用遍历生成html结构的同时绑定点击事件,这样可以节省代码,
+	但是写到给a标签的父级添加自定义索引值的时候不好实现
+
+	第二种思路就是用for循环li 给li设置一个变量用于存a标签的dom元素 在传参到一个函数中 找到li下的a标签添点击事件
+	因为要每一行只能存在一个所以只能通过给点击事情的父级设置变量来存,判断这个变量是否有值,有的话就清空
+	接下来就是要将点击的a标签的元素存入对象当中,然后遍历里面的数据添加到对应的html结构中
+	因为1-9的思路是点击的时候直接找到对应li添加内容,而这种思路是存入数组后,在遍历出所有的内容
+	对象有可能不是按循序来遍历的 所以在处理key为数值的时候我们可以用for来遍历
 
  */
 
-function(){
-	if(){
-		return
-	}
-	else {
-		return
-	}
-}
-function (){
 
+window.onload=function(){
+	var crumbData=[
+		{
+				"title": "品牌",
+				"data": [ "苹果","小米","锤子","魅族","华为","三星","OPPO","vivo","乐视"]
+			},
+			{
+				"title": "尺寸",
+				"data": ["4.0-4.5英寸","4.6-4.9英寸","5.0-5.5英寸","6.0英寸以上"]
+			},
+			{
+				"title": "系统",
+				"data": ["android","ios","window phone","无","其他"]
+			},
+			{
+				"title": "网络",
+				"data": ["联通3G","双卡单4G","双卡双4G","联通4G"]
+			}
+	];
+
+	var datelen=crumbData.length;  /*数据的长度*/
+	var ul=document.querySelector('.screenArea');
+	for(var i=0;i<datelen;i++){
+		var li=document.createElement('li');
+		var span=document.createElement('span');
+		span.innerHTML=crumbData[i].title+':';
+		// console.log(span.innerHTML)
+		li.appendChild(span);
+		for(var j=0;j<crumbData[i].data.length;j++){
+			var a=document.createElement('a');
+			a.innerText=crumbData[i].data[j];
+			// console.log(a.innerText)
+			a.href="javascript:;"
+
+		/*	a.onclick=screenA; */  
+			li.appendChild(a);
+		}
+		ul.appendChild(li);
+	}
+
+
+	// function screenA(){
+	// 	console.log(this)
+	// 	var li=this.parentNode;  
+
+	// 	this.className='active';
+	// }
+	
+	var li=document.querySelectorAll('.screenArea li');
+	for(var i=0;i<li.length;i++){
+		li[i].Acolor;     //*在循环的时候给li设置变量*/
+		screenA(li[i],i);
+	}
+	var shuju={};
+	function screenA(li,n){
+		console.log(li.Acolor)
+		var a=li.querySelectorAll('a');
+		for(var i=0;i<a.length;i++){
+			a[i].onclick=function(){
+				var fLi=this.parentNode; /*找到父级li*/
+				if(fLi.Acolor){
+					fLi.Acolor.className='';
+				}
+				this.className='active';
+				fLi.Acolor=this;
+				shuju[n]=this.innerText;   /*点击的值存入对象当中*/
+				
+				objshuju();
+			}
+		}
+	}
+
+	/* 生成对象数据*/
+	function objshuju(){	
+		/*对象有可能不是按循序来遍历的 所以在处理key为数值的时候我们可以用for来遍历*/
+		
+		for(var attr in shuju){
+			console.log(attr)
+		}
+	}
 }
--- dom基础/js/Dom1-9.js
+++ dom基础/js/Dom1-9.js
@@ -5,8 +5,73 @@
  * @version $Id$
  */
 
-/*
-	将每次点击a标签的值存入对象当中 同个li下只能存一个值
-	有一定顺序
 
- */
+/*
+		看需求是否需要前后端进行分离  如果需要前后分离的话需要从后端拿到josn类型的字符生成html结构
+		需求:点击a标签 出现对应的div 每一列a标签只能出现一个 并且按每行li的顺序
+		出现div的li直接在html中写死
+		
+		通过找到每个li 封装一个函数 传入每个li的dom元素以及数值
+		找到对应的a标签加点击事件 当前隐藏其他显示 设置一个变量存当前点击的adom元素 给下一次使用
+		获取到span的内容，创建一个div赋值,那么重点来了因为之前在html写死了三个li放div
+		所以只要通过传入li的数值找到,并且添加,每次添加之前要清空li里面的内容
+
+
+	 */
+
+window.onload=function(){
+
+	var list=document.querySelectorAll('.list');
+
+	for(var i=0;i<list.length;i++){
+		
+	
+		List(list[i],i);
+	}
+	var selectli=document.querySelectorAll('#select li');
+	console.log(selectli);
+	function List(li,n){	
+		var a=li.querySelectorAll('a');
+		var A;/*存一个值用于下一次显示*/
+
+		for(var i=0;i<a.length;i++){
+			a[i].onclick=function(){
+				
+				if(A){
+					A.parentNode.style.display='block';	
+				}
+				this.parentNode.style.display='none';
+				A=this;
+
+				/*方案1设置一个变量用于存放上一次点击的值 判断这个变量是否有值*/
+
+
+				/*
+				方案2设置一个变量并且赋值A=a[0] 
+				这种方案就不用判断因为变量一开始就是有值的具体代码如下
+				 A.parentNode.style.display='block';
+				 this.parentNode.style.display='none';
+				 A=this;
+
+				*/
+				var span=this.parentNode.parentNode.previousElementSibling;
+				console.log(span);
+
+				selectli[n].innerHTML='<div><span>'+span.innerHTML+'</span><em>'+this.innerText+'</em><a href="javascript:;">X</a></div>';
+
+				
+				console.log(n)
+				var dele=selectli[n].querySelector('a');
+				console.log(dele)
+
+				dele.onclick=function(){
+					dele.parentNode.parentNode.innerHTML='';
+					console.log(A)
+					A.parentNode.style.display = 'block';
+				}
+			}
+
+		}
+
+	}
+}
