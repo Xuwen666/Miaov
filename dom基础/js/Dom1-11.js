ec014c4d453a240fbf26444caa5a280ca54ddea5
xuwen6662 <1956957897@qq.com>
2018-04-16 18:03:07 +0800

    4-16 18:03

--- dom基础/js/Dom1-11.js
+++ dom基础/js/Dom1-11.js
@@ -5,7 +5,38 @@
  * @version $Id$
  */
 
+/*
 
+	需求:
+		 1.
+		 根据京东后台来自动生成html结构
+		 思路1：创建一个li 创建一个p 创建一个div 
+		 创建一个input,div里面创建两个span
+ */
 window.onload=function(){
-	
+	var List=document.querySelector('.emailList .list');
+	console.log(list)
+	for(var i=0;i<list.length;i++){
+		var li=document.createElement('li');
+		var input=document.createElement('input');
+		input.type='checkbox';
+
+		var p=document.createElement('p');
+		p.innerText=list[i].desc;
+		var div=document.createElement('div');
+		div.className='text';
+		div.className='clear';
+		console.log(div)
+		var span1=document.createElement('span');
+		var span2=document.createElement('span');
+		span1.innerHTML=list[i].caption;
+		span1.innerHTML=list[i].time;
+		console.log(list[i].caption)
+		div.appendChild(span1);
+		div.appendChild(span2);
+		li.appendChild(input);
+		li.appendChild(div);
+		li.appendChild(p);
+		List.appendChild(li)
+	}
 }
---
+++ dom基础/js/data.js
@@ -0,0 +1,32 @@
+let list = [
+	{
+		"id": "0",
+		"caption": "添加邮件1",
+		"time": "2018-4-16",
+		"desc": "WebAssembly.Studio is an online IDE"
+	},
+	{
+		"id": "1",
+		"caption": "添加邮件2",
+		"time": "2018-4-16",
+		"desc": "WebAssembly.Studio is an online IDE"
+	},
+	{
+		"id": "2",
+		"caption": "添加邮件3",
+		"time": "2018-4-16",
+		"desc": "WebAssembly.Studio is an online IDE"
+	},
+	{
+		"id": "3",
+		"caption": "添加邮件4",
+		"time": "2018-4-16",
+		"desc": "WebAssembly.Studio is an online IDE"
+	},
+	{
+		"id": "4",
+		"caption": "添加邮件5",
+		"time": "2018-4-16",
+		"desc": "WebAssembly.Studio is an online IDE"
+	}
+]
